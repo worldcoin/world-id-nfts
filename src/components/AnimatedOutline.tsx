23b4df9fa14d53b436ceb3ec06b5d035ceeaf107
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-04-15 07:52:39 +0400

    chore: design fixes (#5)

--- src/components/AnimatedOutline.tsx
+++ src/components/AnimatedOutline.tsx
@@ -3,11 +3,8 @@ import { PropsWithChildren } from 'react'
 
 export function AnimatedOutline({ children, className }: PropsWithChildren<{ className?: string }>) {
 	return (
-		<div className={cn('relative p-0.5 rounded-full overflow-clip', className)}>
-			<span className="absolute inset-0  bg-[var(--tw-gradient-from)]" />
-			<span className="absolute inset-0  bg-[var(--tw-gradient-to)] animate-outline" />
-
-			<div className="relative p-0.5 bg-sand-300 rounded-[inherit]">{children}</div>
+		<div className={cn('relative p-0.5 rounded-full animate-outline', className)}>
+			<div className="relative p-0.5 bg-sand-300 rounded-[inherit] opacity-100">{children}</div>
 		</div>
 	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames'
 import Image from 'next/image'
 import { useEffect } from 'react'
 import { Flows, Steps } from 'types'
+import { useModal } from 'connectkit'
 import Icon from '../components/Icon'
 import { Direct } from '@/scenes/Direct'
 import WorldieNFT from '@/abi/WorldieNFT.abi'
@@ -29,6 +30,16 @@ export const Main: FC = () => {
 
 	const { address: wagmiAddr } = useAccount()
 	const { disconnect } = useDisconnect()
+
+	const connectKitModal = useModal()
+
+	useEffect(() => {
+		if (flow === Flows.connectKit && !connectKitModal.open) {
+			console.log('test')
+			setFlow(null)
+			setStep(Steps.selectClaimMethod)
+		}
+	}, [connectKitModal, flow])
 
 	useEffect(() => {
 		if (!wagmiAddr) return
@@ -118,11 +129,11 @@ export const Main: FC = () => {
 							<h1 className="text-4xl lg:text-6xl lg:inline">Worldie meets Worldcoin</h1>
 
 							<span className="inline-block lg:text-xl text-grey-700 [vertical-align:top]">
-								in Tokyo 🇯🇵
+								in Tokyo <Icon name="japan" className="w-6 h-6" noMask />
 							</span>
 						</div>
 
-						{step === Steps.intro && (
+						{(step === Steps.intro || step === Steps.selectClaimMethod) && (
 							<Button
 								className="flex gap-3 items-center w-full lg:w-auto justify-center"
 								onClick={() => setStep(Steps.selectClaimMethod)}
